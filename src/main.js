b67a16920478bf37ad5a538d5441d0c3736b2e0a
111 <15521636332@163.com>
2020-02-14 16:07:21 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+import jquery from "./jquery_min.js";
+import bootstrap from "bootstrap-3.3.7-dist/js/bootstrap.min.js";
+import "./src/style.css";
+import "./src/buttons.css";
+import "./src/bootstrap-3.3.7-dist/css/bootstrap.min.css";
